f54984f69fe203ba8d9a9001a516beb30dd91fb1
BrysonXiao <40129652+BrysonXiao@users.noreply.github.com>
2021-05-08 11:20:31 -0700

    Dog Card

--- src/components/DogCard.js
+++ src/components/DogCard.js
@@ -4,9 +4,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent';
+// import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
+// import Typography from '@material-ui/core/Typography';
 
 
 const useStyles = makeStyles({
@@ -20,6 +20,11 @@ const useStyles = makeStyles({
 });
 
 function DogCard(props){
+    /*
+        MAIN PROPS:
+        imageURL
+        breed
+    */
     const classes = useStyles();
 
     return (
@@ -27,22 +32,23 @@ function DogCard(props){
         className={classes.root} 
         raised={true} 
         style={{
-            backgroundColor: props.selected ? '#FFED7A' : '#FFE',
+            backgroundColor: '#FFED7A',
         }}>
-        <CardActionArea onClick={() => props.onClick(props.beenum)}>
-          <CardHeader 
-            title={props.name}
-            subheader={props.scientific}/>
+        <CardActionArea onClick={() => {}}>
           <CardMedia
             className={classes.media}
-            image={props.picture}
-            title={props.name}
+            image={props.imageURL}
+            title={props.breed}
           />
-          <CardContent>
+          <CardHeader 
+            title={props.breed}
+            // subheader={props.secondaryName}
+            />
+          {/* <CardContent>
             <Typography variant="body2" color="textSecondary" component="p">
               {props.description}
             </Typography>
-          </CardContent>
+          </CardContent> */}
         </CardActionArea>
       </Card>
     );
@@ -50,4 +56,4 @@ function DogCard(props){
 
 
 
-export default BeeInfo;
+export default DogCard;

e67465168e55b87211a9236842d71a8b92f8249c
PrateekS1917 <61072843+PrateekSane@users.noreply.github.com>
2021-05-08 11:50:14 -0700

    display things

--- src/homepage.js
+++ src/homepage.js
@@ -1,27 +1,49 @@
-import './App.css';
+import "./App.css";
 import React from "react";
+import DogCard from "./components/DogCard";
 
 class Homepage extends React.Component {
-
-	constructor(props) {
-		super(props);
-		this.state = {
-			searchQuery: "",
-		};
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchQuery: "",
+      data: [],
+    };
 
     this.submitQuery = this.submitQuery.bind(this);
     this.displayQuery = this.displayQuery.bind(this);
-	}
+  }
 
-	submitQuery(event) {
+  async hot_callAPI(breedName) {
+    const hot_opts = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        "x-api-key": "6b74986d-b833-4e61-b172-ba23c9802406",
+      },
+    };
+
+    const hot_response = await fetch(
+      "https://api.thedogapi.com/v1/breeds/",
+      hot_opts
+    );
+
+    if (!hot_response.ok) throw new Error(hot_response.status);
+
+    const hot_json = await hot_response.json();
+
+    const hot_rand = parseInt(Math.random() * hot_json.length);
+    const hot_img = hot_json[hot_rand].image.url;
+    this.setState({ hotimg: [...this.state.data, hot_json[hot_rand]] });
+    console.log(hot_img);
+  }
+
+  submitQuery(event) {
     // Something else
     event.preventDefault();
-    alert(this.state.searchQuery);
-
+    this.hot_callAPI();
     // clear out input
-    this.setState({
-      searchQuery: ""
-    });
   }
 
   displayQuery(event) {
@@ -31,14 +53,14 @@ class Homepage extends React.Component {
     });
   }
 
-	renderHeader() {
-		const name = "Dogginator";
-		return <h className="Header">{name}</h>;
-	}
+  renderHeader() {
+    const name = "Dogginator";
+    return <h className="Header">{name}</h>;
+  }
 
-	renderSearchBar() {
-		return (
-			<form className="Search" onSubmit={this.submitQuery}>
+  renderSearchBar() {
+    return (
+      <form className="Search" onSubmit={this.submitQuery}>
         <label>
           <input
             type="text"
@@ -46,20 +68,23 @@ class Homepage extends React.Component {
             value={this.state.searchQuery}
             onChange={this.displayQuery}
           />
-        </label>&nbsp;
+        </label>
+        &nbsp;
         <input type="submit" value="Go!" />
       </form>
-		);
-	}
-
-	render() {
-		return(
-			<div className="Homepage">
-					{this.renderHeader()}
-					{this.renderSearchBar()}
-			</div>
-		);
-	}
+    );
+  }
+  render() {
+    const displayDogs = this.state.data.map((cur) => {
+      return <DogCard imageURL={cur.image.url} />;
+    });
+    return (
+      <div className="Homepage">
+        {this.renderHeader()}
+        {this.renderSearchBar()}
+      </div>
+    );
+  }
 }
 
 export default Homepage;
